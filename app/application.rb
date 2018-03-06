aba6295c4771613c1fca2733b5478d59ddec7e5a
Chely Ho <adept_inept@yahoo.com>
2018-03-06 15:25:34 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new
 
     time = Time.new
-    noon = DateTime.new (time.year, time.month, time.day, 12, 0)
+    noon = DateTime.new (time.year, time.month, time.day, 12)
     resp.write time < noon ? "Good Morning!" : "Good Afternoon!"
 
     resp.finish
