e6735c555440098772289c98d5fa335b76efc481
Chely Ho <adept_inept@yahoo.com>
2018-03-06 15:27:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,10 +1,11 @@
+require 'Date'
 class Application
 
   def call(env)
     resp = Rack::Response.new
 
     time = Time.new
-    noon = DateTime.new (time.year, time.month, time.day, 12)
+    noon = DateTime.new(time.year, time.month, time.day, 12)
     resp.write time < noon ? "Good Morning!" : "Good Afternoon!"
 
     resp.finish
