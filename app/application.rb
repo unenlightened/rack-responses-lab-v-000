42f545b394d928d94941067f970f0ec2a01ca11b
Chely Ho <adept_inept@yahoo.com>
2018-03-06 15:24:03 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,7 +5,7 @@ class Application
 
     time = Time.new
     noon = DateTime.new (time.year, time.month, time.day, 12, 0)
-    resp.write time.hour <= 12 ? "Good Morning!" : "Good Afternoon!"
+    resp.write time < noon ? "Good Morning!" : "Good Afternoon!"
 
     resp.finish
   end
